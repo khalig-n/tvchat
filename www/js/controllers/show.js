dfffa25aeca99a797e3fe3b4d5a6cfbd119628ae
Khalig Novruzli <Khalig Novruzli>
2016-10-19 16:33:24 +0300

    Completed

---
+++ www/js/controllers/show.js
@@ -0,0 +1,78 @@
+var mod = angular.module('tvchat.controllers.show', []);
+
+
+
+mod.controller('ShowCtrl', function ($scope,
+                                     $stateParams,
+                                     $firebaseArray,
+                                     $ionicScrollDelegate,
+                                     ShowsService,
+                                     UserService) {
+
+	$scope.user = UserService;
+
+  $scope.showId = $stateParams.showId;
+  $scope.show = ShowsService.getShow(parseInt($scope.showId));
+
+
+	$scope.data = {
+		messages: [],
+		message: '',
+		loading: true,
+		showInfo: false
+	};
+
+	var messagesRef = firebase.database().ref();
+
+	$scope.loadMessages = function () {
+
+    console.log("Loading data for show", $scope.show);
+
+    var query = messagesRef
+      .child("messages")
+      .orderByChild("showId")
+      .equalTo($scope.showId)
+      .limitToLast(200);
+
+    $scope.data.messages = $firebaseArray(query);
+    $scope.data.messages.$loaded().then(function (data) {
+      console.log("AngularFire $loaded");
+      $scope.data.loading = false;
+      $ionicScrollDelegate.$getByHandle('show-page').scrollBottom(true);
+
+    });
+
+	};
+
+	$scope.sendMessage = function () {
+
+	  if ($scope.data.message)
+    {
+      $scope.data.messages.$add({
+        showId  : $scope.showId,
+        text  : $scope.data.message,
+        username  : firebase.auth().currentUser.displayName,
+        userId  : firebase.auth().currentUser.uid,
+        profilePic  : firebase.auth().currentUser.photoURL,
+        timestamp : new Date().getTime()
+      });
+
+      $scope.data.message='';
+      $ionicScrollDelegate.$getByHandle('show-page').scrollBottom(true);
+    }
+
+	};
+
+	$scope.loadMessages();
+
+	console.log("ShowCtrl-Created");
+
+	$scope.$on("$ionicView.enter", function () {
+		console.log("ShowCtrl-Enter");
+	});
+
+	$scope.$on("$ionicView.beforeLeave", function () {
+		console.log("ShowCtrl-Leave");
+	});
+
+});
